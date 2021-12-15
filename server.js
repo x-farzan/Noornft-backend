33565fa5f0ba6f44cbf697da893682833c322700
Farzan Hassan <farzan.hassan@optimusfox.com>
2021-12-15 07:54:58 +0500

    added nft scheme and posting price and _id in it

--- server.js
+++ server.js
@@ -53,12 +53,31 @@ app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
-var corsOptions = {
-  origin: "https://noor-nft.herokuapp.com",
-  optionsSuccessStatus: 200, // For legacy browser support
-};
+app.use(cors());
+app.options("*", cors());
 
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent// to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middlewarenext();
+  next();
+});
 
 // Define Routes
 
