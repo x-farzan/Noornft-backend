8583f4a3afea0f13d7453fe0b7db310780a69b8a
Farzan Hassan <farzan.hassan@optimusfox.com>
2022-01-13 17:23:43 +0500

    cleaning master

--- helpers/deleteRecord.js
+++
@@ -1,3 +0,0 @@
-// exports.deleteRecord = async (id) => {
-  
-// };
--- helpers/userFieldsValidator.js
+++
@@ -1,9 +0,0 @@
-exports.userFieldsValidator = (fields, req) => {
-  let errors = [];
-  fields.forEach((field) => {
-    if (req[field] === "" || req[field] === null || req[field] === undefined) {
-      return errors.push(field + " is required");
-    }
-  });
-  return errors;
-};
